d2df5274f7fe6a43f952c9b5f252f6e0fa5ad13b
hedgehog9 <redpwon@gmail.com>
2025-05-22 22:18:17 +0900

    포인트 충전, 사용에 대한 정책 추가

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -6,7 +6,8 @@ public record UserPoint(
         long updateMillis
 ) {
 
-    public static final long MAX_POINT = 100_000;
+    private static final long MAX_POINT = 100_000;
+    private static final long MIN_USE_CHARGE_POINT = 1L;
 
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
@@ -18,10 +19,10 @@ public record UserPoint(
      * @return
      */
     public UserPoint use(long amount) {
-        if (amount > this.point) {
+        if (amount < MIN_USE_CHARGE_POINT) {
+            throw new IllegalArgumentException("포인트 사용은 1 Point 이상 사용 가능합니다.");
+        }else if (amount > this.point) {
             throw new IllegalArgumentException("사용할 포인트가 보유중인 포인트보다 많습니다.");
-        } else if (amount == 0) {
-            throw new IllegalArgumentException("사용할 포인트가 올바르지 않습니다.");
         }
 
         return new UserPoint(this.id, this.point - amount, System.currentTimeMillis());
@@ -33,7 +34,10 @@ public record UserPoint(
      * @return
      */
     public UserPoint charge(long amount) {
-        if(MAX_POINT <= this.point + amount) {
+        if (amount < MIN_USE_CHARGE_POINT) {
+            throw new IllegalArgumentException("포인트 충전은 1 Point 이상 충전 가능합니다.");
+        }
+        else if(MAX_POINT <= this.point + amount) {
             throw new IllegalArgumentException("보유할 수 있는 최대 포인트는 100,000 포인트 입니다.");
         }
         return new UserPoint(this.id, this.point + amount, System.currentTimeMillis());
